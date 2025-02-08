ac1e1c4040a01899f69aa27876cebf94bc317ab0
abardashevich <abardashevich@gmail.com>
2025-02-08 04:36:56 +0100

    fix commit conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,25 @@
-import pytest
-import os
-from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selene import browser
+import os
 
 @pytest.fixture(scope='function')
 def setup_browser():
-    """Фикстура для настройки браузера с проверкой переменной CI"""
-
     chrome_options = Options()
 
-    # Проверка переменной окружения CI для активации headless режима
+    # Включаем headless режим, если переменная окружения CI установлена в true
     if os.getenv('CI', 'false').lower() == 'true':
-        chrome_options.add_argument('--headless')  # Включаем headless режим в CI
+        chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
-    browser.config.driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=chrome_options
-    )
+    # Используем кастомный путь для кеша драйвера
+    driver_path = ChromeDriverManager(path='/tmp/wdm').install()
 
+    browser.config.driver = webdriver.Chrome(service=Service(driver_path), options=chrome_options)
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.timeout = 10
 
     yield browser
 
