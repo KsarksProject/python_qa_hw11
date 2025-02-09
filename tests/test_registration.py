062638e32fdd23b2b98d43ac0f0400e601261d85
abardashevich <abardashevich@gmail.com>
2025-02-09 21:42:30 +0100

    All is working + allure

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,16 @@
+import allure
+from allure_commons.types import Severity
 from pages.registration_page import RegistrationPage
 from models.user import User
 
 
+@allure.tag('web')
+@allure.feature("Practice form")
+@allure.story("Проверка регистрации")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "Aleksandr Bardashevich")
+@allure.description("Тест проверяет регистрацию")
+@allure.link("https://demoqa.com", name="demoqa.com")
 def test_registration_form(browser_management):
     user = User(
         first_name='Linus',
@@ -35,11 +44,11 @@ def test_registration_form(browser_management):
 
     registration_page = RegistrationPage()
 
-    # Открываем страницу регистрации
-    registration_page.open()
+    with allure.step("Открываем страницу регистрации"):
+        registration_page.open()
 
-    # Регистрируем пользователя
-    registration_page.register(user)
+    with allure.step("Заполняем форму регистрации и отправляем данные"):
+        registration_page.register(user)
 
-    # Проверяем результат
-    registration_page.should_have_registered(expected_results)
+    with allure.step("Проверяем, что данные регистрации отображаются корректно"):
+        registration_page.should_have_registered(expected_results)
