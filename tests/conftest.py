c1fd9e58351b23948d05f2c588308c3044d2624b
abardashevich <abardashevich@gmail.com>
2025-02-18 00:37:05 +0100

    DONE

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from utils import allure_attach
 
 
 @pytest.fixture(scope="function")
-def setup_browser():
+def browser_management():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -15,14 +16,24 @@ def setup_browser():
             "enableVideo": True
         }
     }
+
     options.capabilities.update(selenoid_capabilities)
 
+    # Создаем драйвер с базовой авторизацией в URL (рабочий вариант)
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    browser = Browser(Config(driver=driver))
+    browser.config.driver = driver
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
     yield browser
+
+    allure_attach.add_screenshot(browser)
+    allure_attach.add_logs(browser)
+    allure_attach.add_html(browser)
+    allure_attach.add_video(browser)
 
     browser.quit()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,57 +1,58 @@
 import allure
 from pages.registration_page import PracticeFormPage
-from data.resources import path
 
 @allure.title("Успешное заполнение формы")
-def test_practice_form(setup_browser):
-    with allure.step("Открытие формы регистрации"):
-        practice_form_page = PracticeFormPage()
-        practice_form_page.open()
+def test_practice_form(browser_management):
+    practice_form = PracticeFormPage()
+
+    # Открытие и заполнение формы
+    with allure.step("Открытие страницы регистрации"):
+        practice_form.open()
 
     with allure.step("Заполнение полного имени"):
-        practice_form_page.fill_first_name("Linus")
-        practice_form_page.fill_last_name("Torvalds")
+        (practice_form.fill_first_name("Linus")
+                     .fill_last_name("Torvalds"))
 
     with allure.step("Заполнение email"):
-        practice_form_page.fill_email("torvalds@osdl.org")
+        practice_form.fill_email("torvalds@osdl.org")
 
     with allure.step("Выбор пола"):
-        practice_form_page.select_gender("Male")
+        practice_form.select_gender("Male")
 
     with allure.step("Заполнение номера телефона"):
-        practice_form_page.fill_user_number("9876543210")
+        practice_form.fill_user_number("9876543210")
 
     with allure.step("Выбор даты рождения"):
-        practice_form_page.pick_date_of_birth("1969", "December", "28")
+        practice_form.pick_date_of_birth("1969", "December", "28")
 
     with allure.step("Выбор интересующих предметов"):
-        practice_form_page.fill_subject("Accounting")
-        practice_form_page.fill_subject("Maths")
+        (practice_form.fill_subject("Accounting")
+                     .fill_subject("Maths"))
 
     with allure.step("Выбор хобби"):
-        practice_form_page.choose_interest_reading()
+        practice_form.choose_interest_reading()
 
     with allure.step("Добавление картинки"):
-        practice_form_page.upload_picture(path("contact.jpg"))
+        practice_form.upload_picture("contact.jpg")
 
     with allure.step("Заполнение полного адреса"):
-        practice_form_page.fill_address("123, Open Source Development Labs")
-        practice_form_page.choose_state("NCR")
-        practice_form_page.choose_city("Delhi")
+        (practice_form.fill_address("123, Open Source Development Labs")
+                     .choose_state("NCR")
+                     .choose_city("Delhi"))
 
     with allure.step("Отправка формы регистрации"):
-        practice_form_page.submit_button()
+        practice_form.submit_button()
 
     with allure.step("Сравнение отправленных и переданных значений"):
-        practice_form_page.should_registered_user_with(
-            "Linus Torvalds",
-            "torvalds@osdl.org",
-            "Male",
-            "9876543210",
-            "28 December,1969",
-            "Accounting, Maths",
-            "Reading",
-            "contact.jpg",
-            "123, Open Source Development Labs",
-            "NCR Delhi"
+        practice_form.should_registered_user_with(
+            full_name="Linus Torvalds",
+            email="torvalds@osdl.org",
+            gender="Male",
+            user_number="9876543210",
+            birthdate="28 December,1969",
+            subjects="Accounting, Maths",
+            hobby="Reading",
+            file="contact.jpg",
+            address="123, Open Source Development Labs",
+            location="NCR Delhi"
         )
